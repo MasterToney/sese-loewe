475f5e324bdfee0d97d91358a321750b2338421b
atmaxinger <maxi6594@gmail.com>
2018-10-29 11:27:33 +0100

    Add maven and angular project

---
+++ server/src/main/java/sese/ws2018/server/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package sese.ws2018.server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
