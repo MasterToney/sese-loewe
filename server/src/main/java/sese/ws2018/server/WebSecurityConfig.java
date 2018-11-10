5d26c3d3e82b0b0fa28df3caab86ae719869f5ce
atmaxinger <maxi6594@gmail.com>
2018-11-10 22:53:37 +0100

    configured spring-security to be more flexible

---
+++ server/src/main/java/sese/ws2018/server/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package sese.ws2018.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/*
+    https://spring.io/guides/gs/securing-web/
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/api/*").authenticated()
+                .antMatchers("/**").permitAll()
+            .and()
+            .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
